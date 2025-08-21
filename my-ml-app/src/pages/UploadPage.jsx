d951007bd1d4dafafa8e09b43e3e8d96c1217c4d
kimsokeen <keenmeka@gmail.com>
2025-08-21 15:28:27 +0700

    Update UploadPage.jsx

--- my-ml-app/src/pages/UploadPage.jsx
+++ my-ml-app/src/pages/UploadPage.jsx
@@ -114,7 +114,6 @@ function UploadPage() {
         <div style={styles.resultBox}>
           <h3>Prediction Result</h3>
           <p><strong>Status:</strong> {response.prediction}</p>
-          <p><strong>Confidence level:</strong> {response.confidence_score}</p>
           {response.wound_area_pixels !== undefined && (
             <>
               {response.wound_area_cm2 && (
